805f6d4c681be9bba31b4ba78b39bcf183b778af
erbkaiser <6014923+erbkaiser@users.noreply.github.com>
2023-03-13 23:04:15 +0100

    Add beta to main for testing

---
+++ beta/fc_userscript_loader.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Frozen Cookies
+// @version        github-latest
+// @description    Userscript to load Frozen Cookies
+// @author         Icehawk78 / erbkaiser
+// @homepage       https://github.erbkaiser.com/FrozenCookies/
+// @include        http://orteil.dashnet.org/cookieclicker/
+// @include        https://orteil.dashnet.org/cookieclicker/
+// @updateURL      https://github.erbkaiser.com/FrozenCookies/fc_userscript_loader.user.js
+// @downloadURL    https://github.erbkaiser.com/FrozenCookies/fc_userscript_loader.user.js
+// ==/UserScript==
+
+// Source:    https://github.com/erbkaiser/FrozenCookies/main/
+// Github.io: https://erbkaiser.github.io/FrozenCookies/
+var loadInterval = setInterval(function () {
+    const Game = unsafeWindow.Game;
+    if (Game && Game.ready) {
+        clearInterval(loadInterval);
+        Game.LoadMod("https://github.erbkaiser.com/FrozenCookies/beta/frozen_cookies.js");
+    }
+}, 1000);
