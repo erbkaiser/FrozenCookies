6621d259b7f0e4e868a5af366a65ec8cab4737e9
erbkaiser <6014923+erbkaiser@users.noreply.github.com>
2023-03-28 12:22:45 +0200

    Sync beta

--- beta/fc_preferences.js
+++ beta/fc_preferences.js
@@ -20,11 +20,12 @@ FrozenCookies.preferenceValues = {
         display: ["Autoclick GC OFF", "Autoclick GC ON"],
         default: 0,
     },
-    autoWC: {
-        hint: "Automatically click Wrath Cookies when they appear (does nothing if autoGC is OFF)",
-        display: ["Autoclick Wrath Cookies OFF", "Autoclick Wrath Cookies ON"],
-        default: 1,
-    },
+    // Not working right
+    // autoWC: {
+    //    hint: "Automatically click Wrath Cookies when they appear (does nothing if autoGC is OFF)",
+    //    display: ["Autoclick Wrath Cookies OFF", "Autoclick Wrath Cookies ON"],
+    //    default: 1,
+    // },
     autoReindeer: {
         hint: "Automatically click reindeer",
         display: ["Autoclick Reindeer OFF", "Autoclick Reindeer ON"],
